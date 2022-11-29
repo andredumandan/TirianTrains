a30acdcbe94554f4081e93b86cc1a49a21df903f
Andre Dumandan <84612277+andredumandan@users.noreply.github.com>
2022-11-30 00:02:01 +0800

    Added 4 new test queries to test_queries.sql

--- test_queries.sql
+++ test_queries.sql
@@ -25,3 +25,24 @@
 --     JOIN ticket ON ticket.ticket_id = ticket_trip.ticket_id
 --     WHERE ticket.ticket_id = 1
 -- );
+
+
+-- CUSTOMERS VIEW
+SELECT CONCAT(last_name, ' ', given_name) AS "NAME", birth_date AS "BIRTHDAY"
+FROM customer;
+
+-- TRIP SCHEDULE VIEW
+SELECT trip_date AS "DATE", COUNT(trip_id) AS "NO. OF TRIPS"
+FROM trip
+GROUP BY trip_date;
+
+-- TRAINS VIEW
+SELECT train_id AS "TRAIN ID", CONCAT(series,'-',model_number) AS "MODEL"
+FROM train;
+
+-- TRAIN MAINTENANCE HISTORY VIEW
+SELECT cert.maintenance_date AS "DATE", CONCAT(crew.last_name,' ', crew.given_name) AS "CREW IN CHARGE", cert.task AS "TASKS",cert.condition AS "CONDITION"
+FROM crew_certificate cert, maintenance_crew crew, maintenance_certificate mc
+WHERE cert.crew_no=crew.crew_no
+AND cert.certificate_no=mc.certificate_no
+ORDER BY cert.maintenance_date;
