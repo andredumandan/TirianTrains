8d5dfd58b0f2f0e61a64249b91c50fadc62e1585
josemaria.layug@obf.ateneo.edu <josemaria.layug@obf.ateneo.edu>
2022-12-02 06:10:34 +0800

    Added more data entries

--- populate_customers.sql
+++ populate_customers.sql
@@ -70,3 +70,132 @@ VALUES (
     'Female'
 );
 
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Maypi',
+    'Sokaba',
+    'B',
+    '03/03/2003',
+    'Female'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Tim',
+    'An',
+    'G',
+    '25/12/2002',
+    'Male'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Lig',
+    'Ma',
+    'D',
+    '15/02/1992',
+    'Female'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Ayla',
+    'By',
+    'U',
+    '05/10/1998',
+    'Female'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Imee',
+    'Sy',
+    'U',
+    '23/09/2006',
+    'Other'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Ayo',
+    'Kon',
+    'A',
+    '23/09/1989',
+    'Female'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Hyu',
+    'Yu',
+    'U',
+    '29/01/2001',
+    'Male'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Yas',
+    'Su',
+    'O',
+    '13/02/2003',
+    'Male'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Vai',
+    'Pe',
+    'R',
+    '01/05/1999',
+    'Female'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Dee',
+    'Va',
+    'H',
+    '08/05/2001',
+    'Male'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Loe',
+    'Ba',
+    'H',
+    '02/08/1998',
+    'Female'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Emily',
+    'Dickinson',
+    'M',
+    '14/02/1980',
+    'Other'
+);
+
+INSERT INTO customer
+VALUES (
+    nextval('customer_customer_id_seq'),
+    'Ge',
+    'ge',
+    'G',
+    '11/03/1988',
+    'Other'
+);
